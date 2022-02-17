edb83bd43f67fae0bb137b8cd3cb6cff999f5cb6
Destiny Walcott <dwalcot1@binghamton.edu>
2022-02-17 14:40:31 -0500

    working on parser

--- submit/prj1-sol/parse.sh
+++ submit/prj1-sol/parse.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 #simply add the command you use to run your parser
+
+./parser.mjs
--- submit/prj1-sol/parser.mjs
+++ submit/prj1-sol/parser.mjs
@@ -5,6 +5,7 @@ import Path from 'path';
 
 function parse(text) {
   const tokens = scan(text);
+  console.log(tokens);
   let index = 0;
   let lookahead = nextToken();
   const value = program();
@@ -22,104 +23,112 @@ function parse(text) {
     }
   }
 
-  function nextToken() { return (index >= tokens.length) ? tokens.push(['EOF', '<EOF>']) : tokens[index++]; }
+  function nextToken() {
+    return (
+      (index >= tokens.length) ? tokens.push(['EOF', '<EOF>']) : tokens[index++]
+    );
+  }
+
+  function program() {
+    const asts = [];
+    while (!peek('EOF')) {
+      asts.push(source());
+      //match(lookahead[0]);
+    }
+    return asts;
+  }
 
   function source() {
     let result = [];
     while(peek('TEXT') || peek('IFDEF') || peek('IFNDEF')) {
-      const kind = lookahead[0];
-      const value = lookahead[1];
-      match(kind);
-      if (kind === 'TEXT') { 
-	const ast = new Ast('TEXT');
-	ast.text = value;    
-        result.append(ast); 
+      console.log("in source", lookahead);
+      let kind = lookahead[0];
+      let lexeme = lookahead[1];
+      
+      if (kind === 'TEXT') {
+	match(kind);
+        result.push(new AstText(lexeme)); 
       }
       else {
-	const res = ifdef();
-	result.push(res);
+	
       }
     }
     return result;
   }
 
-  function ifdef() {
+  function ifdef() {    
     let result = [];
+    result.push(...source());
+    
+    while(peek('elif')) {
+      match('elif');
 
-    let kind = lookahead[0];
-    if (kind === 'IFDEF' || kind == 'IFNDEF') { 
-      match(kind);
-
+      const kind = lookahead[0];
       const value = lookahead[1];
-      match('SYM');
-      const ast = new Ast(kind, source());
-      ast.sym = value;
-      //ast.xkids = source();
-      result.append(ast);
-    }
+      //match(kind);
 
-    while(peek('ELIF')) {
-      kind = lookahead[0];
-      match(kind);
-      
-      const value = lookahead[1];
-      match('SYM');
-      const ast = Ast(kind, source());
-      ast.sym = value;
-      result.append(ast);
+      if (kind === 'sym') {
+	match();
+        result.push(new ast('elif', value, ...source()))
+      }
     }  
     
-    kind = lookahead[0];
-    if (kind === 'ELSE') {
-      const ast = new Ast(kind, source());
-      result.append(ast);
+    const kind = lookahead[0];
+    if (kind === 'else') {
+      match(kind);
+      result.push(new ast(kind, null, ...source()));
     }
+
+    match('endif');
 
     return result;
   }
 
 }
-
 
 function scan(input) {
   let text = "";
   const tokens = [];
 
   input = input.split('\n');
+  input.pop();
   while (input.length > 0) {
     let m;
-    if ((m = input[0].match(/^\s+/))) {
-      // ignore whitespace
+    if ((m = input[0].match(/^\s*[a-zA-Z]+/))) {
+      let newText = m.input + '\n';
+      text = text.concat(newText);
     }
-    else if ((m = input[0].match(/^[a-zA-Z]+/))) {
-      text.concat(' ', m);
-    }
-    else {
-      m = input[0].match(/^\#/);
-
+    else if ((m = input[0].match(/^\s*\#i[a-zA-Z]+|\#e[a-zA-Z]+/))) {
       if (text.length > 0) {
-        tokens.push(['TEXT', text]);
+        tokens.push(['TEXT', text]); 
         text = "";
       }
-
-      directive = m.split(' ')[0];
-      if (directive.length < 2) text.concat(' ', m);
-      else {
-	tokens.push([directive.slice(1).toUpperCase(), directive]);
-        if (directive == '#ifdef' || directive == '#ifndef' || directive == '#elif') {
-          for (const str of m.split(' ')) {
-	    let s;
-	    if ((s = str.match(/^[_a-zA-Z]\w*/))) tokens.push(['SYM', s]);
-	  }
-	}
+      
+      let directive = m[0].trim();
+      tokens.push([directive.slice(1).toUpperCase(), directive]);
+      if (directive == '#ifdef' || directive == '#ifndef' || directive == '#elif') {
+        let str = m.input.trim().split(' ')[1];
+	if (str && (m = str.match(/^[_a-zA-Z]\w*/))) tokens.push(['SYM', m[0]]);
       }
+    }
+    else {
+    	m = input[0].match(/^\s*.*|\s*/);
+	if (m) {
+          let newText = m.input + '\n'
+          text = text.concat(newText);
+	}
     }
 
     input = input.slice(1);
   }
+
+  if (text.length > 0) {
+    tokens.push(['TEXT', text]); 
+    text = "";
+  }
+  
   return tokens;
 }
-
 
 const CHAR_SET = 'utf8';
 function main() {
@@ -128,9 +137,16 @@ function main() {
   console.log(JSON.stringify(value));
 }
 
+class AstText {
+  constructor(TXT) {
+    Object.assign(this, {tag: "TEXT", text: TXT});
+  }
+}
+
 class Ast {
-  constructor(tag, ...kids) {
-    Object.assign(this, {tag, xkids: kids});
+  constructor(tag, sym, ...kids) {
+    if (sym != null) Object.assign(this, {tag, sym, xkids: kids});
+    else Object.assign(this, {tag, xkids: kids});
   }
 }
 main();
