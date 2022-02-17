3a2f34aae2e7fa61ca86743f1db90e2ef88ceea4
Destiny Walcott <dwalcot1@binghamton.edu>
2022-02-17 09:20:18 -0500

    done with lexer

--- submit/prj1-sol/lexer.mjs
+++ submit/prj1-sol/lexer.mjs
@@ -8,14 +8,14 @@ function scan(input) {
   const tokens = [];
 
   input = input.split('\n');
+  input.pop();
   while (input.length > 0) {
     let m;
     if ((m = input[0].match(/^\s*[a-zA-Z]+/))) {
       let newText = m.input + '\n';
-      if (text.length < 1) text = newText;
-      else text = text.concat(' ', newText);
+      text = text.concat(newText);
     }
-    else if ((m = input[0].match(/^\s*\#[a-zA-Z]+/))) {
+    else if ((m = input[0].match(/^\s*\#i[a-zA-Z]+|\#e[a-zA-Z]+/))) {
       if (text.length > 0) {
         tokens.push(['TEXT', text]); 
         text = "";
@@ -25,12 +25,15 @@ function scan(input) {
       tokens.push([directive.slice(1).toUpperCase(), directive]);
       if (directive == '#ifdef' || directive == '#ifndef' || directive == '#elif') {
         let str = m.input.trim().split(' ')[1];
-	if ((m = str.match(/^[_a-zA-Z]\w*/))) tokens.push(['SYM', m[0]]);
+	if (str && (m = str.match(/^[_a-zA-Z]\w*/))) tokens.push(['SYM', m[0]]);
       }
     }
     else {
-    	m = input[0].match(/^\s*.*[^.]/);
-	if (m) tokens.push(['TEXT', m.input + '\n']);
+    	m = input[0].match(/^\s*.*|\s*/);
+	if (m) {
+          let newText = m.input + '\n'
+          text = text.concat(newText);
+	}
     }
 
     input = input.slice(1);
@@ -41,15 +44,14 @@ function scan(input) {
     text = "";
   }
   
-  return JSON.stringify(tokens);
+  return tokens;
 }
 
 const CHAR_SET = 'utf8';
 function main() {
   const text = fs.readFileSync(0, CHAR_SET);
   const tokens = scan(text);
-  console.log(tokens);
-  //fs.writeFileSync(1, tokens);
+  console.log(JSON.stringify(tokens));
 }
 
 main();
