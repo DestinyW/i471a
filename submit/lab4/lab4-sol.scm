2ee7a75b9893280d32a86378aed50305e7a204bb
Destiny Walcott <dwalcot1@binghamton.edu>
2022-02-24 14:40:52 -0500

    working on exercise 2

--- submit/lab4/lab4-sol.scm
+++ submit/lab4/lab4-sol.scm
@@ -1,12 +1,19 @@
-(define (quadratic-roots a b c)
-  (if (= a 0)
-    'error
-    (let ((discriminant (sqrt (- (expt b 2) (* 4 a c)))))
-      (list (/ (+ (- b) discriminant) (* 2 a)) (/ (- (- b) discriminant) (* 2 a))))))
+(define quadratic-roots
+  (lambda (a b c (func sqrt))
+    (if (= a 0)
+      'error
+      (let ((discriminant (func (- (expt b 2) (* 4 a c)))))
+	(list (/ (+ (- b) discriminant) (* 2 a)) (/ (- (- b) discriminant) (* 2 a)))))))
 
 (define my-sqrt
-  (lambda (n (guess 1))
+  (lambda (n (guess 1.0))
     (let ((val (/ (abs (- (* guess guess) n)) n)))
       (if (<= val 0.0001)
 	guess
 	(my-sqrt n (/ (+ guess (/ n guess)) 2))))))
+
+(define (cmp-gt ls1 ls2)
+   (if (null? ls1)
+       '()
+       (cons (> (car ls1) (car ls2))
+             (cmp-gt (cdr ls1) (cdr ls2)))))
