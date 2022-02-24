7454f36870f899c4ee82c8dba467b5dc72607156
Destiny Walcott <dwalcot1@binghamton.edu>
2022-02-24 13:11:38 -0500

    still working on exercise 1

--- submit/lab4/lab4-sol.scm
+++ submit/lab4/lab4-sol.scm
@@ -4,3 +4,9 @@
     (let ((discriminant (sqrt (- (expt b 2) (* 4 a c)))))
       (list (/ (+ (- b) discriminant) (* 2 a)) (/ (- (- b) discriminant) (* 2 a))))))
 
+(define my-sqrt
+  (lambda (n (guess 1))
+    (let ((val (/ (abs (- (* guess guess) n)) n)))
+      (if (<= val 0.0001)
+	guess
+	(my-sqrt n (/ (+ guess (/ n guess)) 2))))))
