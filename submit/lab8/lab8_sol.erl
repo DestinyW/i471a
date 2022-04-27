15d252a730685c42dff2492800bb26b8c5f154f3
Destiny Walcott <dwalcot1@binghamton.edu>
2022-04-26 22:39:41 -0400

    done with exercises 1 & 2

--- submit/lab8/lab8_sol.erl
+++ submit/lab8/lab8_sol.erl
@@ -20,5 +20,45 @@ grades_data() ->
 
 %% Exercise 2
 
+perimeter({square, Side}) ->
+  4 * Side;
+perimeter({circle, Radius}) ->
+  2 * 3.14159 * Radius.
+
+guard_perimeter({Type, L}) when Type =:= square ->
+  4 * L;
+guard_perimeter({Type, L}) when Type =:= circle ->
+  2 * 3.14159 * L.
+
+if_perimeter({Type, L}) ->
+  if Type =:= square -> 4 * L;
+      Type =:= circle -> 2 * 3.14159 * L
+  end.
+
+case_perimeter(Shape) ->
+  case Shape of
+    {square, Side} -> 4 * Side;
+    {circle, Radius} -> 2 * 3.14159 * Radius
+  end.
+
+letter_grade(Points) when Points > 90 -> 
+  'A';
+letter_grade(Points) when Points =< 90, Points > 80 -> 
+  'B';
+letter_grade(Points) when Points =< 80, Points > 70 -> 
+  'C';
+letter_grade(Points) when Points =< 70, Points > 60 -> 
+  'D';
+letter_grade(Points) when Points =< 60 -> 
+  'F'.
+
+if_letter_grade(Points) ->
+  if Points > 90 -> 'A';
+      Points =< 90, Points > 80 -> 'B';
+      Points =< 80, Points > 70 -> 'C';
+      Points =< 70, Points > 60 -> 'D';
+      Points =< 60 -> 'F'
+  end.
+
 %% Exercise 3.
 
